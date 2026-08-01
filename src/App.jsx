52a2f483c92c284096bc5a24d20433fd1b3b13bf
tahirabatool218-uoe <tahirabatool218@gmail.com>
2026-08-01 12:45:37 +0500

    Fix import path for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero/Hero"
-import Navbar from "./components/navbar/navbar"
+import Navbar from "./components/Navbar/navbar"
 import DestinationSection from "./components/Destination/DestinationSection"
 import ServiceCard from "./components/Services/ServiceCard"
 import ServicesSection from "./components/Services/ServicesSection"
