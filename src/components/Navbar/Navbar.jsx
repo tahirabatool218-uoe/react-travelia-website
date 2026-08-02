fb394cd75e2555f4c88bd0fde4f9cc122b4abdb5
tahirabatool218-uoe <tahirabatool218@gmail.com>
2026-08-02 10:19:51 +0500

    Improve navbar styling and responsive design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import './Navbar.css'
 import { HiMenu } from 'react-icons/hi'
 import { FaGlobeAmericas } from 'react-icons/fa'
 function Navbar() {
   const [menuOpen,setMenuOpen]=useState(false)
+  const [activeSection,setActiveSection]=useState("home");
   return (
    <header className='header'>
    <nav className='navbar'>
@@ -19,23 +20,48 @@ function Navbar() {
    
     <ul className={menuOpen ? "navlinks active" : "navlinks"}>
         <li>
-            <a href="#home" onClick={() => setMenuOpen(false)}>Home</a>
+            <a href="#home" className={activeSection === "home" ? "active": ""} 
+            onClick={() => {
+              setMenuOpen(false)
+              setActiveSection("home");
+              }}>Home</a>
         </li>
 
           <li>
-            <a href="#destination" onClick={() => setMenuOpen(false)}>Destination</a>
+            <a href="#destinations"  className={activeSection === "destinations" ? "active": ""} 
+            onClick={() => {
+              setMenuOpen(false)
+              setActiveSection("destinations");
+            }
+              }>Destination</a>
         </li>
 
         <li>
-            <a href="#services" onClick={() => setMenuOpen(false)}>Services</a>
+            <a href="#services" className={activeSection === "services" ? "active": ""} 
+            onClick={() =>
+            {
+              setMenuOpen(false)
+              setActiveSection("services");
+            
+            }}>Services</a>
         </li>
 
           <li>
-            <a href="#about" onClick={() => setMenuOpen(false)} >About</a>
+            <a href="#about" className={activeSection === "about" ? "active": ""} 
+            onClick={() =>{
+               setMenuOpen(false)
+              setActiveSection("about");
+              }}
+                >About</a>
         </li>
 
           <li>
-            <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
+            <a href="#contact" className={activeSection === "contact" ? "active": ""} 
+            onClick={() => {
+            setMenuOpen(false)
+            setActiveSection("contact");
+            }
+            }>Contact</a>
         </li>
     </ul>
 <div className="nav-right">
